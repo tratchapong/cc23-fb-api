bebfe4fe5e81a9427d023db0eda1bb0921673e58
tratchapong <tratchapong@gmail.com>
2026-08-03 16:57:39 +0700

    shutdown util ok

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv'
 import app from './app.js'
+import shutdown from './utils/shutdown.util.js'
 
 dotenv.config()
 
@@ -8,5 +9,12 @@ const PORT = process.env.PORT || 8000
 app.listen(PORT, () => {
 	console.log('Server on Port', PORT)
 })
+
+// Listen for termination signals
+process.on('SIGINT', () => shutdown('SIGINT'));   // Ctrl+C
+process.on('SIGTERM', () => shutdown('SIGTERM')); // kill command or Docker stop
+// Catch unhandled errors
+process.on("uncaughtException", ()=>  shutdown('uncaughtException'))
+process.on("unhandledRejection", ()=> shutdown('unhandledRejection'))
 
 
