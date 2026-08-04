fa1a5685a65f0b83e0c0ad178dddca651d80cd1f
tratchapong <tratchapong@gmail.com>
2026-08-04 16:44:06 +0700

    jwt error handle in error-middleware

--- src/middlewares/error.middleware.js
+++ src/middlewares/error.middleware.js
@@ -1,14 +1,28 @@
-import {z, ZodError} from 'zod'
+import { z, ZodError } from 'zod'
 
 export default function (err, req, res, next) {
 	// console.log("Have Error!!!\n", err)
 
-	if (err instanceof ZodError ) {
+	if (err.name === 'TokenExpiredError') {
+		return res.status(401).json({
+			error: 'Token Expired',
+			message: 'Your session has expired. Please log in again.'
+		});
+	}
+
+	if (err.name === 'JsonWebTokenError') {
+		return res.status(401).json({
+			error: 'Invalid Token',
+			message: 'The provided token is invalid or malformed.'
+		});
+	}
+
+	if (err instanceof ZodError) {
 		console.log('Zod Error :', z.flattenError(err))
 		return res.status(400).json({
 			success: false,
-			message : 'Validation Error',
-			errors : z.flattenError(err).fieldErrors
+			message: 'Validation Error',
+			errors: z.flattenError(err).fieldErrors
 			// errors: err.issues
 			// errors: err.issues.map(err => err.message)
 		})
