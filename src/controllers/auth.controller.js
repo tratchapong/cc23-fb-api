05ac62df548419d15dad2a4a487216d51a54af6d
tratchapong <tratchapong@gmail.com>
2026-08-04 11:15:18 +0700

    register ok with zod validation

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,17 +1,48 @@
+import bcrypt from 'bcryptjs'
+import { prisma } from '../lib/prisma.js'
 import createHttpError from 'http-errors'
+import identityKeyUtil from '../utils/identity-key.util.js'
+import { registerSchema } from '../validations/schema.js'
 
+export async function register(req, res, next) {
+  const { identity, firstName, lastName, password, confirmPassword } = req.body
+  // validation
+  const data = registerSchema.parse(req.body)
 
-export function register(req, res, next) {
-	 res.send('Register Controller')
+  // check Identity is email or mobile
+  const identityKey = identityKeyUtil(identity)
+  if (!identityKey) {
+    return next(createHttpError[400]('identity must be email or phone number'))
+  }
+  // find user for non-duplicate
+  const haveUser = await prisma.user.findUnique({
+    where: { [identityKey]: identity }
+  })
+  if (haveUser) {
+    return next(createHttpError[409]('This user already register'))
+  }
+  // create user
+  const newUser = {
+    [identityKey]: identity,
+    password: await bcrypt.hash(password, 10),
+    firstName: firstName,
+    lastName: lastName
+  }
+  const result = await prisma.user.create({ data: newUser })
+  res.json({
+    message: 'Register Successful',
+    result: result
+  })
+
 }
 
 export function login(req, res) {
   res.json({
-    msg : 'Login Controller',
-    body : req.body
+    msg: 'Login Controller',
+    body: req.body
   })
 }
 
 export function getMe(req, res) {
-	res.send('Get me Controller')
+  res.send('Get me Controller')
 }
--- src/middlewares/error.middleware.js
+++ src/middlewares/error.middleware.js
@@ -1,5 +1,14 @@
 export default function (err, req, res, next) {
-	console.log("Have Error!!!\n", err)
+	// console.log("Have Error!!!\n", err)
+
+	if (err.name === 'ZodError') {
+		return res.status(400).json({
+			success: false,
+			// errors: err.issues
+			errors: err.issues.map(err => err.message)
+		})
+	}
+
 	res.status(err.status || 500)
 	res.json({
 		error: err.message
