2f7ae58532ee6876cf443f1ad833351e5757d215
tratchapong <tratchapong@gmail.com>
2026-08-06 17:44:12 +0700

    schema.prisma fix like to likes

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import createHttpError from 'http-errors'
 import notFoundMiddleware from './middlewares/notFound.middleware.js'
 import errorMiddleware from './middlewares/error.middleware.js'
 import authenticateMiddleware from './middlewares/authenticate.middleware.js'
+import postRoute from './routes/post.route.js'
 
 const app = express()
 app.use(cors({
@@ -17,12 +18,7 @@ app.use(cors({
 app.use(express.json())
 
 app.use('/api/auth', authRoute )
-app.use('/api/post', authenticateMiddleware , (req, res)=>{ 
-	res.json({
-		message : 'Post service api',
-		user : req.user
-	}) 
-} )
+app.use('/api/post', authenticateMiddleware, postRoute  )
 
 app.use(notFoundMiddleware)
 
